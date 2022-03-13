b87625d78f8eb9937efdb28854788c5e10b52b03
MarinaKhasanova <mnmatis@gmail.com>
2022-03-13 21:21:49 +0300

    function solveEquation

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,24 @@
+"use strict";
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  let d;
+  d = Math.pow(b,2)-4*a*c;
+  if (d < 0) {
+    arr = [];
+  }  else if (d == 0) {
+      let x = -b/(2*a);
+      arr = [x];
+    }
+    else {
+      let x = (-b + Math.sqrt(d) )/(2*a);
+      let y = (-b - Math.sqrt(d) )/(2*a);
+      arr = [x, y];
+    }
+
   return arr; // array
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
